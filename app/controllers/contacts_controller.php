89aa1c35e4d305564ff5e0093d0da8d824e6ddd5
vvan <volkanvan@gmail.com>
2011-02-07 22:06:21 +1100

    contact us files

--- app/controllers/contacts_controller.php
+++ app/controllers/contacts_controller.php
@@ -1,7 +1,22 @@
 <?php
 
-	class ContactsController extends AppController{
+	class ContactsController extends AppController {
+		var $components = array('Email');
 		var $name = 'Contacts';
-	}
-
+		//var $scaffold;
+		
+		function add() {
+		    if ($this->RequestHandler->isPost()) {
+		        $this->Contact->set($this->data);
+		        if ($this->Contact->validates()) {
+		            //send email using the Email component
+		            $this->Email->to = 'volkanvan@gmail.com';  
+		            $this->Email->subject = 'Contact message from ' . $this->data['Contact']['name'];  
+		            $this->Email->from = $this->data['Contact']['email'];
+		            $this->Email->send($this->data['Contact']['details']);
+		        }
+		    }
+		}
+		
+    }
 ?>
--- app/models/contact.php
+++ app/models/contact.php
@@ -1,8 +1,23 @@
 <?php
-	class Contact extends AppModel{
-		var $name = 'Contact';
-		
-		
+	class Contact extends AppModel {
+	    var $useTable = false;
+	    var $_schema = array(
+	        'name'		=>array('type'=>'string', 'length'=>100), 
+	        'email'		=>array('type'=>'string', 'length'=>255), 
+	        'details'	=>array('type'=>'text')
+	    );
+	
+	    var $validate = array(
+		    'name' => array(
+		        'rule'=>array('minLength', 1), 
+		        'message'=>'Name is required' ),
+		    'email' => array(
+		        'rule'=>'email', 
+		        'message'=>'Must be a valid email address' ),
+		    'details' => array(
+		        'rule'=>array('minLength', 1), 
+		        'message'=>'Feedback is required' )
+		);
 	}
 	
 ?>
